dec3ad385312e2323b14ec6082e9a864fcd79665
MateoDiaz <matdimo01@gmail.com>
2025-07-09 00:42:10 -0500

    ruta-login

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 from routes.routes import routes_p 
 from routes.routes import routes_u
 from routes.routes import routes_r
-from routes.auth_routes import router as auth_router
+from routes.routes import routes_auth
 
 
 app = FastAPI()
@@ -22,7 +22,7 @@ load_dotenv()
 app.include_router(routes_p)
 app.include_router(routes_u)
 app.include_router(routes_r)
-app.include_router(auth_router)
+app.include_router(routes_auth)
 
 app.add_middleware(
     CORSMiddleware,
