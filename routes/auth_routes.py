50fa8e0f11cc7cdc7d1428ca1a9fc2cea7b95fb1
MateoDiaz <matdimo01@gmail.com>
2025-07-08 22:52:01 -0500

    login-changes

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -2,19 +2,17 @@ from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from models.user_model import Usuario  # ORM creado
 from db.db_postgres import get_db_connection  
-from passlib.context import CryptContext
-from pydantic import BaseModel
+from schemas.login_schema import LoginSchema
 
-router = APIRouter(tags=["Auth"])
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-class LoginRequest(BaseModel):
-    correo: str
-    contrasena: str
+router = APIRouter()
 
 @router.post("/login")
-def login(request: LoginRequest, db: Session = Depends(get_db_connection)):
-    user = db.query(Usuario).filter(Usuario.correo == request.correo).first()
-    if not user or not pwd_context.verify(request.contrasena, user.contrasena):
-        raise HTTPException(status_code=401, detail="Credenciales inválidas")
-    return {"message": "Login exitoso", "user_id": user.id}
+def login(data: LoginSchema, db: Session = Depends(get_db_connection)):
+    usuario = db.query(Usuario).filter(Usuario.correo == data.correo).first()
+    if not usuario:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+    
+    if usuario.contrasena != data.contrasena:
+        raise HTTPException(status_code=401, detail="Contraseña incorrecta")
+    
+    return {"mensaje": "Inicio de sesión exitoso", "usuario_id": usuario.id}
